53069161501ab216f693a89cdc8c5e660c66efe2
Abbey <abbeytn.cs@gmail.com>
2022-09-25 15:55:55 +0700

    update gia tri

--- src/main/java/com/bharath/clinicals/controllers/ClinicalDataController.java
+++ src/main/java/com/bharath/clinicals/controllers/ClinicalDataController.java
@@ -25,9 +25,9 @@ import com.bharath.clinicals.repos.PatientRepository;
 public class ClinicalDataController {
 	int a= 0;
 	int b=1;
-	int c= 2;
+	int c= 7;
 	int d =3;
-	int e =4;
+	int e =10;
 	
 	@Autowired
 	private ClinicalDataRepository clinicalDataRepository;
